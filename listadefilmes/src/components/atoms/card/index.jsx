c327e9f40e58c6ea9ecc9cf4c6dab2b9123d8da9
unknown <diegoreis.dvlopr@gmail.com>
2023-04-03 18:40:38 -0300

    Inserindo put e finalizando projeto

--- listadefilmes/src/components/atoms/card/index.jsx
+++ listadefilmes/src/components/atoms/card/index.jsx
@@ -4,11 +4,14 @@ function Card(props){
     
     
     return(
-        <div className="card">
-            <div className="imagem"></div>
+        <div className="card" onClick={props.onclickCard}>
+            <div className="imagem">
+                <img src={props.cartaz}></img>
+            </div>
             <div className="titulo">
                 <h2>{props.titulo}</h2>
             </div>
+            <p>{props.texto}</p>
             <div className="delete" onClick={props.onclick}>
                 <h3>X</h3>
             </div>
